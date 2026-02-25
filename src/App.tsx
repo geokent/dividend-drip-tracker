e9160209539cb34fda06e87403ca853bd0f7f4b8
geokent <georgekent@gmail.com>
2026-02-24 22:30:22 -0800

    Revert "Test: Add comment to App.tsx (macOS sed fix)"
    This reverts commit 976713647e9a7552a36927db59ced8acaee58d8b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// Test comment - Hero
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
